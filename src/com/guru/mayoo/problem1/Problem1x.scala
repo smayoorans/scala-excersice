6bf4b4ea17cfc4d449cee3ab7f3aa849b9e61c8b
Mayooran <mayooran@hsenidmobile.com>
2013-10-15 16:32:16 +0530

    Problem 1 optimized

---
+++ src/com/guru/mayoo/problem1/Problem1x.scala
@@ -0,0 +1,18 @@
+package com.guru.mayoo.problem1
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mayooran
+ * Date: 10/15/13
+ * Time: 4:06 PM
+ * To change this template use File | Settings | File Templates.
+ */
+object Problem1x {
+  def main(args: Array[String]) {
+       getMultiples(10);
+  }
+
+  def getMultiples(num : Int){
+    println(((1 to num-1).filter(n => n % 3 == 0 || n % 5 == 0)).sum)
+  }
+}
